76fefebed71a36005e6e7f03c7918fe9e5943f00
jameslawler <services@jameslawler.com>
2023-11-26 20:57:53 +0000

    Initial version

---
+++ docs/overview.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+slug: /
+---
+
+# Overview
+
+A collection of my notes on various topics.
+
+## Topics
+
+[Portuguese Language](/category/portuguese) - My notes from studying the Portuguese language (european).
---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "James Lawler's Notes",
+  tagline: "James Lawler's Notes",
+  favicon: "img/favicon.ico",
+  url: "https://jameslawler.github.io",
+  baseUrl: "/notes",
+  organizationName: "jameslawler",
+  projectName: "notes",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
+          editUrl:
+            "https://github.com/jameslawler/notes/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "My Notes",
+        items: [
+          {
+            href: "https://github.com/jameslawler/notes",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Portuguese Language",
+                to: "/category/portuguese",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/jameslawler/notes",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} James Lawler, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
